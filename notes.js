17c22df8751f4cbfad4631d0ff2e305285341042
vkoshta92 <114298415+vkoshta92@users.noreply.github.com>
2024-04-02 22:16:52 +0530

    backend

---
+++ notes.js
@@ -0,0 +1,110 @@
+// https://app.eraser.io/dashboard/all
+// https://www.datensen.com/download.html
+// https://mongoosejs.com/
+// https://github.com/codespaces/new
+// https://stackblitz.com/edit/stackblitz-starters-fer6gp?description=&file=README.md&title=Express%20Starter
+// https://stackblitz.com/edit/stackblitz-starters-fer6gp?file=models%2Fecommerce%2Forder.models.js
+
+// user
+// import mongoose from 'mongoose';
+
+// const userSchema = new mongoose.Schema(
+
+//   {
+//   // username:String,
+//   // email:String,
+//   // isActive:Boolean
+//   username: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     lowercase: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     lowercase: true,
+//   },
+//   password: {
+//     type: String,
+//     // required:[true,"password is required"]
+//     required: true,
+//   },
+// },
+
+// {timestamps:true}
+
+
+
+// );
+
+// export const User = mongoose.model('User', userSchema);
+
+// // small letter plural database me
+// // users.
+
+
+
+
+// // todos
+// import mongoose from "mongoose";
+
+// const todoSchema= new mongoose.Schema(
+//   {
+// content:{
+//   type:String,
+//   requred:true,
+// },
+// complete:{
+//   type:Boolean,
+//   defaultfalse
+// },
+// createdBy:{
+//   type:mongoose.Schema.Types.ObjectId,
+//   ref:"User"
+// },
+// // array of sub todos
+// subTodos:[
+//   {
+//     type:mongoose.Schema.Types.ObjectId,
+//     ref:"SubTodos"
+//   }
+// ]
+//   }
+//   ,{timestamps:true})
+
+// export const Todo=mongoose.model("Todo",todoSchema)
+// // todos in lower case in plural in data base
+
+
+// sub todos
+
+// import mongoose from "mongoose";
+
+// const subTodos=new mongoose.Schema({
+
+//   content:{
+//     type:String,
+//     required:true
+//   },
+//   complete:{
+//     type:Boolean,
+//     default:false
+//   },
+//   createdBy:{
+//     type:mongoose.Schema.Types.ObjectId,
+//     ref:"User",
+//   }
+
+
+// },{timestamps:true})
+
+
+// export const SubTodos=mongoose.model("SubTodos",subTodos)
+
+
+
+
+
+// 
